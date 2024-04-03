1a102f1775d86eb8c4f46947c3fe0bc2e6791f51
Matias Kamelman <125071787+kamelmat@users.noreply.github.com>
2024-04-03 16:07:21 +0000

    feat Conditional Profile Card Kamelmat

--- src/js/app.js
+++ src/js/app.js
@@ -29,18 +29,22 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let social= `<ul class="position-right"${variables.socialMediaPosition}"/></ul>`;
+  if (variables.includeSocialMediaPosition == false) social = "<ul class='position-right'></ul>";
+
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name? variables.name : ""}</h1>
+          <h2>${variables.role? variables.role : "sin experiencia"}</h2>
+          <h3>${variables.city? variables.city : ""} ${variables.country? variables.country : ""}</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href=${variables.twitter? variables.twitter : ""}><i class="fab fa-twitter"></i></a></li>
+            <li><a href=${variables.github? variables.github : ""}><i class="fab fa-github"></i></a></li>
+            <li><a href=${variables.linkedin? variables.linkedin : ""}><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=${variables.instagram? variables.instagram : ""}><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
